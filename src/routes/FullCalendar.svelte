e2ef58bd998a527ce9b7b566e0d4f965e8c770f1
Laszlo Adam <laszloadam08@gmail.com>
2023-09-09 22:05:41 +0200

    dialog-elkeszitese

---
+++ src/routes/FullCalendar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { Calendar } from '@fullcalendar/core';
+	import dayGridPlugin from '@fullcalendar/daygrid';
+
+	onMount(() => {
+		const calendarEl = document.getElementById('calendar');
+		if (calendarEl) {
+			let calendar: Calendar = new Calendar(calendarEl, {
+				plugins: [dayGridPlugin],
+				initialView: 'dayGridMonth',
+				locale: 'hu',
+				timeZone: 'local',
+				firstDay: 1
+			});
+
+			calendar.render();
+		}
+	});
+</script>
+
+<div class="d-flex justify-content-center">
+	<div id="calendar" />
+</div>
+
+<style>
+	#calendar {
+		width: 50%;
+		height: 50%;
+	}
+</style>
