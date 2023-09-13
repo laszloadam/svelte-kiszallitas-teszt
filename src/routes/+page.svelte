1c2cadabf6e5c81a7a182bb385d791b3e9f15ac5
László Ádám <66565320+laszloadam@users.noreply.github.com>
2023-09-13 14:53:57 +0200

    Revert "Feature/crud letrehozas"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import FullCalendar from '$lib/components/FullCalendar.svelte';
+
 </script>
 
-<h1 class="text-center mt-3">Kiszállítási naptár</h1>
+<div>
+Hello world!
+</div>
 
-<FullCalendar />
+<style>
+	
+</style>
